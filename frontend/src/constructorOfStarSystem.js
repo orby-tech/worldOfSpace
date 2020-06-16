be51662d04ee3482f68f80ac0c770ed14b1c2829
orby-tech <tibode495@gmail.com>
2020-06-16 23:20:44 +0300

    new mechancs  of constrictor

--- frontend/src/constructorOfStarSystem.js
+++ frontend/src/constructorOfStarSystem.js
@@ -2,101 +2,89 @@ import React, { Component ,Link }  from 'react';
 import  { connect } from 'react-redux'
 import  sun from "./img/sun.png";
 const a =  3
-let center = []
-let stars = [[120, 120, 1, 0, 0], [140, 140, 1, 0, 0]]
 class PREConstructorStarSystem extends Component{
+
     constructor(props){
       super(props);
+      
       this.newStar = this.newStar.bind(this)
-      this.coordinatsUpdate = this.coordinatsUpdate.bind(this)
+
+
+      this.center = []
+      this.stars = [[220, 220, 3, 0, 0], [420, 420, 1, -0.1, 0]]
+      this.timer = 0 
       this.state = {
         work: true,
-        stars: [[120, 120, 1, 0, 0], [420, 420, 1, 0, 0]],
+        stars: [[120, 120, 1, 10, 10], [140, 140, 1, 1, 1]],
         tick: 1,
         
       }
   
     }
 
-    newCoordinats(asteroid, i) {
-
-      return asteroid
-    }
   
-    coordinatsUpdate() {
-        let arr = stars
-        let centerX = center[0]
-        let centerY = center[1]
+    coordinatsUpdate() {      
+        let arr = this.stars
+        let distance = 0
+        let power = 0
+        let powerX = 0
+        let powerY = 0
+        let temp = []
         for (let i = 0; i< arr.length; i++) {
-            arr[i][4] += a *  (1 / arr[i][3]) ** 1.5
-            arr[i][0] = centerX + arr[i][3] * Math.cos(arr[i][4] )
-            arr[i][1] = centerY + arr[i][3] * Math.sin(arr[i][4] )
+          for (let j = i+1; j< arr.length; j++) {            
+            distance = ((arr[i][0] - arr[j][0]) ** 2 + (arr[i][1] - arr[j][1]) ** 2) ** 0.5            
+            power = arr[i][2] * arr[j][2] / distance * 0.0005
+     
+            powerX = power / distance  * (arr[i][0] - arr[j][0])
+            powerY = power / distance  * (arr[i][1] - arr[j][1])
+            arr[i][3] -= powerX / arr[i][2]
+            arr[i][4] -= powerY / arr[i][2]
+            arr[j][3] += powerX / arr[j][2]
+            arr[j][4] += powerY / arr[j][2]
+            if (  distance < 0.5) {
+              let mass = arr[i][2] + arr[j][2]
+              let x = (arr[i][3] * arr[i][2] + arr[j][3] * arr[j][2]) / mass
+              let y = (arr[i][4] * arr[i][2] + arr[j][4] * arr[j][2]) / mass
+              arr[i] = [ arr[i][0], arr[i][1], mass, x, y ]
+              arr[j] = [null, null, 1, null, null]
+            } 
+          }
         }
-        stars = arr
-        this.setState({stars: arr})
-        setTimeout(() => {
-          this.coordinatsUpdate()
-        }, 1);    
+        let tempArr = []
+        for (let i = 0; i< arr.length; i++) {
+          if (arr[i][0] && Math.abs(arr[i][0])< 1000 && Math.abs(arr[i][1])< 1000){
+            arr[i][0] += arr[i][3]
+            arr[i][1] += arr[i][4]
+
+
+            tempArr.push(arr[i])
+          }
+        }
+        this.stars = tempArr
+        this.setState({stars: tempArr})
+         
     }
     componentDidMount(){
-
-      this.countCenterOfMass()
-      setTimeout(() => {
-        this.coordinatsUpdate()
-      }, 1000);
-
-      document.addEventListener("click", this.newStar);
-    }
-    centerMassMashine ( arr ) {
-      let centerX = 0
-      let centerY = 0
-      let allMass = 0
-      for (let i = 0; i< arr.length; i++) {
-          centerX += arr[i][0] * arr[i][2]
-          centerY += arr[i][1] * arr[i][2]
-          allMass += arr[i][2]
+      if(document.querySelector('.constructorOfStarSystem__star')) {
+        this.timer = setInterval(() => {
+          
+      this.coordinatsUpdate() 
+        }, 1);
+        document.addEventListener("click", this.newStar);
+      } else {
+        clearInterval(this.timer)
       }
-
-      centerX /= allMass
-      centerY /= allMass
-
-
-      center = [centerX, centerY]
     }
-    coordinatsOfStars() {
-      let arr = stars
-      let centerX = center[0]
-      let centerY = center[1]
-      for (let i = 0; i< arr.length; i++) {
-        arr[i][3] = ((arr[i][0] - centerX) ** 2 + (arr[i][1] - centerY) ** 2) ** 0.5 
-        arr[i][4] = Math.abs(Math.atan ( (arr[i][1] - centerY)/(arr[i][0] - centerX)))
-        if( arr[i][0] - centerX > 0 && arr[i][1] - centerY > 0 ){
-            arr[i][4] += Math.PI / 2 * 3
-        } 
-        if( arr[i][0] - centerX < 0 && arr[i][1] - centerY < 0 ){
-            arr[i][4] += Math.PI / 2
-        }
-        if( arr[i][0] - centerX < 0  && arr[i][1] - centerY > 0 ){
-            arr[i][4] += Math.PI
-        } 
-        if( arr[i][0] - centerX > 0  &&  arr[i][1] - centerY < 0 ){
-            arr[i][4] += 0
-        }    
-        arr[i][4] += Math.PI / 2     
-      }
-      stars = arr
-    }
-    countCenterOfMass(){
-        this.centerMassMashine(stars)
 
-        this.coordinatsOfStars()
-    }
 
     newStar(event) {
-        let arr = stars
-        arr.push([event.x, event.y, 1, 0, 0])
-        stars = arr
-        this.countCenterOfMass()
+      clearInterval(this.timer)
+        let arr = this.stars
+        arr.push([event.x, event.y, 3, (Math.random() - 0.5) / 10 , (Math.random() - 0.5) / 10])
+        this.stars = arr
+        this.timer = setInterval(() => {
+          this.coordinatsUpdate()
+        }, 1);
         
     }
   
@@ -109,10 +97,14 @@ class PREConstructorStarSystem extends Component{
                 <img  
                   className="spaceObject constructorOfStarSystem__star"
                   title="Звезда класса G"
-                  style={{left: star[0], top: star[1]}}
+                  style={{left: star[0], top: star[1], width: Math.cos(star[2] / 200) * 10 }}
                   alt="sun IMG"
                   src={sun}/>
-               
+                <div className="constructorOfStarSystem__changer">
+                  <div className="constructorOfStarSystem__changerItem">
+
+                  </div>
+                </div>
                 
               </>
             )}
